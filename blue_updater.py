7f903bdfdd5192197cd5a661525b1d544cb43fa3
mgeraert <marc.geraerts@gmail.com>
2022-01-30 14:44:10 +0100

    Updater via shell v2

--- blue_updater.py
+++ blue_updater.py
@@ -8,6 +8,8 @@ updatr = Blueprint('updater', __name__)
 def updater():
     return render_template('updater.html')
 
+
+
 @updatr.route("/update")
 def update():
     os.system('/etc/MLDali/updateMLDali.sh')
